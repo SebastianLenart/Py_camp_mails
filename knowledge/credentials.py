34a32311bbd861b2c31764209f6da87f5cdb1300
SebastianLenart <stentbike7@gmail.com>
2023-06-28 23:58:54 +0200

    intro

--- knowledge/credentials.py
+++ knowledge/credentials.py
@@ -12,6 +12,27 @@ for message_id in message_ids[1][0].split():
     tuple1, tuple2 = imap_server.fetch(message_id, "RFC822")
     print(tuple1) # OK
     print(tuple2[0][1]) # TEXT
+    message = email.message_from_bytes(tuple2[0][1])
+    print(message) # bardziej obrobiona wiadomosc bez krzakow
+    print(decode_header(message["Subject"]))
+    print(decode_header(message["From"]))
+    subject, subject_encoding = decode_header(message["Subject"])[0]
+    if subject_encoding is not None:
+        subject = subject.decode("utf-8")
+    email_from, from_encoding = decode_header(message["From"])[0]
+    if from_encoding is not None:
+        email_from = email_from.decode("utf-8")
+    print("v2:", decode_header(message["Subject"]))
+    print("v2:", decode_header(message["From"]))
+
+    # zalaczniki - bloki, pobieranie plikow
+    for chunk in message.walk():
+        if chunk.get_filename() is not None:
+            file = open(chunk.get_filename(), "wb")
+            print(chunk.get_content_maintype(), chunk.get_filename())
+            print(chunk.get_payload(decode=True)) # pobieranie zawartosci pliku
+            # quit()
+
 
 
 
