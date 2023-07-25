77fe94db9cceac28dd752f9de97b5a92dfdb404d
SebastianLenart <stentbike7@gmail.com>
2023-07-25 23:10:56 +0200

    app finish in OOP

--- task/main.py
+++ task/main.py
@@ -13,9 +13,10 @@ server = "imap.gmail.com"
 
 
 class Mail:
-    def __init__(self, subject, contents):
+    def __init__(self, subject):
         self.subject = subject
-        self.contents = contents
+        self.contents = []
+        self.attachment = []
 
 
 class MailBox:
@@ -39,9 +40,14 @@ class MailBox:
         subject, subject_encoding = decode_header(message["Subject"])[0]
         if subject_encoding is not None:
             subject = subject.decode("utf-8")
-        return Mail(
-            subject, message.walk()
-        )
+
+        mail = Mail(subject)
+        for part in message.walk():
+            if part.get_filename() is not None:
+                mail.attachment.append(part)
+            else:
+                mail.contents.append(part)
+        return mail
 
     def get_emails(self):
         mails = []
@@ -49,6 +55,33 @@ class MailBox:
             _, msg = self.imap_server.fetch(message_id, '(RFC822)')
             mails.append(self._parse_email(msg[0][1], ))
         return mails
+
+
+class Filter:
+    def __init__(self, pattern, search_in_content, search_in_attachment_name):
+        self.pattern = r"{}".format(pattern)
+        self.search_in_content = search_in_content
+        self.search_in_attachment_name = search_in_attachment_name
+        self._is_mail_ok = False
+        self.email = None
+
+    def _search_in_content(self, part):
+        return True if re.search(self.pattern, part.as_string(), re.IGNORECASE) else False
+
+    def _search_in_attachment_name(self, part):
+        return part.get_filename() is not None and re.match(self.pattern, part.get_filename(), re.IGNORECASE)
+
+    def check(self):
+        if self.email is None:
+            raise ValueError("Email object is not set!")
+        if self.search_in_content:
+            for part in self.email.contents:
+                self._is_mail_ok = self._search_in_content(part)
+
+        if self.search_in_attachment_name:
+            for attachment in self.email.attachments:
+                self._is_mail_ok = self._search_in_attachment_name(attachment)
+        return self._is_mail_ok
 
 
 @click.command()
@@ -61,6 +94,7 @@ def main(uploads_to, search, search_in_content, search_in_attachment_name):
     print(search_in_attachment_name)
     print(search_in_content)
     os.makedirs(uploads_to, exist_ok=True)
+    filter = Filter(search, search_in_content, search_in_attachment_name)
     with open("config.yaml", "r") as file:
         credentials = yaml.safe_load(file)
         # pprint(credentials)
@@ -69,22 +103,13 @@ def main(uploads_to, search, search_in_content, search_in_attachment_name):
             mailbox.connect()
 
             for email in mailbox.get_emails():
-                is_mail_ok = False
-                for part in email.contents:
-                    if search_in_attachment_name:
-                        search = r"{}".format(search)
-                        if part.get_filename() is not None and re.match(search, part.get_filename(), re.IGNORECASE):
-                            is_mail_ok = True
-                            with open(os.path.join(uploads_to, part.get_filename()), "wb") as file:
-                                file.write(part.get_payload(decode=True))
-                    if search_in_content:
-                        if re.search(search, part.as_string(), re.IGNORECASE):
-                            if "Knedel" in part.as_string():
-                                print(part.as_string())
-                            is_mail_ok = True
-
-                if is_mail_ok:
+                filter.email = email
+                if filter.check():
                     print(credential['login'], email.subject)
+                    for attachment in email.attachments:
+                        with open(os.path.join(uploads_to, attachment.get_filename()), "wb") as file:
+                            file.write(attachment.get_payload(decode=True))
+
 
 
 if __name__ == "__main__":
@@ -98,7 +123,8 @@ python main.py --search .*\.jpg --search-in-attachment-name
 python main.py --search .*\.jpg --search-in-attachment-name --uploads-to=photoJPG # bedzie wrzucał zdj do katalogu photoJPG
 ***
 python main.py --search Has.*o --search-in-content # bo "ł" nie dzialalo!
-python main.py --search "Has(l||=C5=82)0 --search-in-content
+python main.py --search "Has(l||=C5=82)0" --search-in-content
+
 
 """
 
